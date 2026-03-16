5b94487e5ce01f6d3d63e122a879d6301e5713ef
Hrushikesh-0105 <mhrushikesh0105@gmail.com>
2026-03-16 19:30:16 +0530

    Added defualt variables

--- day2/variables.tf
+++ day2/variables.tf
@@ -1,9 +1,11 @@
 variable "filename" {
   description = "Name of the file to create"
   type        = string
+  default     = "default.txt"
 }
 
 variable "message" {
-  description = "Message to write inside the file"
+  description = "Message written in file"
   type        = string
+  default     = "Default environment"
 }
